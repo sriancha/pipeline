f489c6ce97aa6727e7afc24c55edde918b658af7
Srikanth.Ancha <srik2434@gmail.com>
2022-02-09 22:26:01 +0530

    new file

---
+++ Buildfile.groovy
@@ -0,0 +1,24 @@
+pipeline{
+    agent any
+    stages{
+
+        stage("build"){
+            steps {
+                print "hello Good Morning"
+                sh "mvn clean package"
+
+            }
+        
+        }
+        stage("upload"){
+            steps {
+                println "Upload the new file"
+            }
+        }
+        stage("depoly"){
+            steps {
+                println "depoly the code"
+            }
+        }
+    }
+}
