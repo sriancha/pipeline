287ab368d0c50dccf07d4c628b0d08b0ef49154f
Srikanth.Ancha <srik2434@gmail.com>
2022-02-09 22:59:41 +0530

    new file

--- Buildfile.groovy
+++ Buildfile.groovy
@@ -22,7 +22,7 @@ pipeline{
             steps {
                 println "depoly the code"
                 sh "ls -lart"
-                sh "aws s3 cp hello-*.war s3://pipelineart "
+                sh "aws s3 cp target/hello-*.war s3://pipelineart "
             }
         }
     }
