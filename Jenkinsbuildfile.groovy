bc29e0ddedfc024fb76b2e7bbf357f67833c180c
Srikanth.Ancha <srik2434@gmail.com>
2022-02-11 22:07:20 +0530

    new file added

--- Jenkinsbuildfile.groovy
+++ Jenkinsbuildfile.groovy
@@ -1,10 +1,12 @@
 pipeline{
     agent any
+
     stages {
 
-        stage("Clone-branch"){
+        stage("Clone- the Repositry"){
             steps {
-                print "hello Good Morning - clone"
+                print "hello-Good Morning - clone"
+
                 checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'git-auth', url: 'https://github.com/sriancha/GitToday.git']]])
                 sh "ls -lart ./*"
 
@@ -13,6 +15,7 @@ pipeline{
         }
         stage ("Build"){
             steps {
+        
                 println "Upload the new file"
                 sh "mvn clean package"
             }
